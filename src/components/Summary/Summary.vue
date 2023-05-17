05468d3267497a5fde3c040d1ae26fc42d9851cc
amoschoomy <acho0057@student.monash.edu>
2023-05-17 14:43:44 +1000

    update summary.vue for the ability to update summary

--- src/components/Summary/Summary.vue
+++ src/components/Summary/Summary.vue
@@ -1,14 +1,19 @@
 <template>
-    <div>
-        <h1>History of Summaries (Total: {{ summaryCount }})</h1>
-        <div class="grid-container">
-            <div v-for="summary in summaries" :key="summary.content_id" class="grid-item">
-                <h2>{{ summary.title }}</h2>
-                <p>Content ID: {{ summary.content_id }}</p>
-                <p>{{ summary.summary }}</p>
-            </div>
+  <div>
+    <h1>History of Summaries (Total: {{ summaryCount }})</h1>
+    <div class="grid-container">
+      <div v-for="(summary, index) in summaries" :key="summary.content_id" class="grid-item"
+        @click="toggleSelectedSummary(index)">
+        <h2>{{ summary.title }}</h2>
+        <p>Content ID: {{ summary.content_id }}</p>
+        <p>{{ summary.summary }}</p>
+        <div v-if="selectedSummaryIndex === index">
+          <textarea v-model="editingSummary" @click.stop></textarea>
+          <button @click.stop.prevent="updateSummary(summary, editingSummary)">Update</button>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -17,9 +22,9 @@ import { ref, watch } from 'vue';
 import { useStore } from 'vuex';
 
 interface Summary {
-    content_id: number;
-    title: string;
-    summary: string;
+  content_id: number;
+  title: string;
+  summary: string;
 }
 
 export default {
@@ -30,6 +35,7 @@ export default {
     let userSub = ref(null);
     let summaries = ref([] as Summary[]);
     let summaryCount = ref(0);
+    let selectedSummary = ref<Summary | null>(null);
 
     const fetchSummaries = async () => {
       if (!userSub.value) return;
@@ -55,11 +61,36 @@ export default {
       }
     };
 
+    let selectedSummaryIndex = ref<number | null>(null);
+
+    let editingSummary = ref('');
+
+    const toggleSelectedSummary = (index: number) => {
+      if (selectedSummaryIndex.value === index) {
+        selectedSummaryIndex.value = null;
+      } else {
+        selectedSummaryIndex.value = index;
+        editingSummary.value = summaries.value[index].summary;
+      }
+    };
+
+    const updateSummary = async (summary: Summary, newSummary: string) => {
+      if (!summary) return;
+      try {
+        console.log('Updating summary:', summary);
+        await axios.post('http://localhost:3000/update-summary', { new_summary: newSummary, content_id: summary.content_id });
+        summary.summary = newSummary;
+        selectedSummaryIndex.value = null;
+      } catch (error) {
+        console.error('Error updating summary:', error);
+      }
+    };
+
     watch(
       () => store.state.user,
       async (newUser) => {
         if (newUser) {
-            console.log('User logged in:', newUser);
+          console.log('User logged in:', newUser);
           userSub.value = newUser.sub;
           await fetchSummaries();
           await fetchSummaryCount();
@@ -72,21 +103,37 @@ export default {
       userSub,
       summaries,
       summaryCount,
+      toggleSelectedSummary,
+      updateSummary,
+      selectedSummaryIndex,
+      editingSummary,
     };
+
   },
 };
 </script>
   
 <style scoped>
 .grid-container {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-    grid-gap: 20px;
-    color: black;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-gap: 20px;
+  color: black;
 }
 
+
 .grid-item {
-    border: 1px solid #ccc;
-    padding: 20px;
+  border: 1px solid #ccc;
+  padding: 20px;
+  cursor: pointer;
+}
+
+textarea {
+  display: block;
+  width: 100%;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  color: black;
+  /* Add this line */
 }
 </style>
